3d47a6991f6c7432c50a5a3778b922300b536817
Bagoly Zoltán <zoltan.bagoly@gmail.com>
2020-03-13 00:08:38 +0100

    Controller KÉSZ

--- Jegmezo2020/src/jegmezo/Controller.java
+++ Jegmezo2020/src/jegmezo/Controller.java
@@ -6,6 +6,9 @@ import jegmezo.items.*;
 public class Controller {
 	public void startGame() {
 		System.out.println("<Controller.startGame()");
+		
+		GameArea ga = new GameArea();
+		
 		System.out.println(">Controller.startGame()");
 	}
 	
@@ -17,5 +20,6 @@ public class Controller {
 	
 	public static void main(String[] args) {
         System.out.println("A legjobb tárgy a Projlab!! -help ;)");
+        startGame();
     }
 }
--- Jegmezo2020/src/jegmezo/GameArea.java
+++ Jegmezo2020/src/jegmezo/GameArea.java
@@ -6,6 +6,27 @@ import jegmezo.items.*;
 public class GameArea {
 	//Ide kell majd két tömb tárolni
 	
+	public GameArea() {
+		System.out.println("<GameArea.constructor()");
+		
+		System.out.println("Creating and adding different tipes of fields as many times and as randomly as we later decide");
+		
+		HoleField holeField = new HoleField();
+		StableIce sableIce = new StableIce();
+		UnstableIce unstableIce = new UnstableIce();
+		Border border = new Border();
+		
+		addField(holeField);
+		addField(stableIce);
+		addField(unstableIce);
+		addField(border);
+		
+		System.out.println("Creating and adding as many avatars as many are needed");
+
+		Avatar avatar = new Avatar();
+		
+		System.out.println(">GameArea.constructor()");
+	}
 	
 	public void addField(Field f) {
 		System.out.println("<GameArea.addField()");
